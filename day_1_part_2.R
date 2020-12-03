07becb83d9f282056d6aec3a943cf8a2e8701b2b
Brent Crossman <brent.crossman@gmail.com>
2020-12-03 14:35:52 -0500

    Create day_1_part_2.R

---
+++ day_1_part_2.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+data <- read_csv("day1.csv", col_names = FALSE)
+
+expand.grid(data$X1, data$X1, data$X1) %>% 
+  set_names(c("first_num", "second_num", "third_num")) %>% 
+  filter((first_num+second_num+third_num)==2020) %>% 
+  slice(1) %>% 
+  summarize(result = first_num*second_num*third_num) %>% 
+  pull()
