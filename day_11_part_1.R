197a3dca7fb9474e34e05380476418d877604630
Brent Crossman <brent.crossman@gmail.com>
2020-12-11 16:38:41 -0500

    Day 11 Final

--- day_11_part_1.R
+++ day_11_part_1.R
@@ -7,22 +7,31 @@ seats <-
   pivot_longer(-row_id, names_to = "col_id", values_to = "state") %>%
   mutate(col_id = as.numeric(gsub(pattern = "V", replacement = "", col_id)))
 
+lookup_function <- function(row_id_use, col_id_use){
+  
+  sum(seats[seats$row_id>=(row_id_use-1)&
+              seats$row_id<=(row_id_use+1)&
+              seats$col_id>=(col_id_use-1)&
+              seats$col_id<=(col_id_use+1)
+        ,"state"]=="#")
+}
+
+vlookup_function <- Vectorize(lookup_function)
+
 for(i in 1:1000){
-  seats <-
+  df <-
     seats %>%
-    rowwise() %>% 
-    mutate(adj_occ = sum(.[.$row_id>=(row_id-1)&
-                             .$row_id<=(row_id+1)&
-                             .$col_id>=(col_id-1)&
-                             .$col_id<=(col_id+1)
-                           ,"state"]=="#"),
+    mutate(adj_occ = vlookup_function(row_id, col_id),
            state_2 = if_else(state=="L"&adj_occ<1, "#", state),
            state_2 = if_else(state=="#"&adj_occ>=5,"L", state_2))
   
-  if(all(seats$state==seats$state_2)){break}
+  if(all(df$state==df$state_2)){break}
   
-  seats$state <- seats$state_2
+  df$state <- df$state_2
+  seats <- df
   print(paste("count:",i,"occupied_seats:", sum(seats$state=="#")))
+  
+  
 }
 
 sum(seats$state=="#")
--- day_11_part_2.R
+++ day_11_part_2.R
@@ -8,69 +8,90 @@ seats <-
   pivot_longer(-row_id, names_to = "col_id", values_to = "state") %>%
   mutate(col_id = as.numeric(gsub(pattern = "V", replacement = "", col_id)))
 
+left_row_occ_lookup <- function(row_id_use, col_id_use){
+  stri_extract_last_regex(paste0(unlist(seats[seats$row_id==(row_id_use) &
+                                                seats$col_id<(col_id_use)
+                                              ,"state"]),
+                                 collapse = ""), pattern = "L|#")=="#"
+}
+right_row_occ_lookup <- function(row_id_use, col_id_use){ 
+  stri_extract_first_regex(paste0(unlist(seats[seats$row_id==(row_id_use) &
+                                                 seats$col_id>(col_id_use)
+                                               ,"state"]),
+                                  collapse = ""), pattern = "L|#")=="#"}
+up_col_occ_lookup <- function(row_id_use, col_id_use){  
+  stri_extract_last_regex(paste0(unlist(seats[seats$row_id<(row_id_use) &
+                                                seats$col_id==(col_id_use)
+                                              ,"state"]),
+                                 collapse = ""), pattern = "L|#")=="#"}
+down_col_occ_lookup <- function(row_id_use, col_id_use){ 
+  stri_extract_first_regex(paste0(unlist(seats[seats$row_id>(row_id_use) &
+                                                 seats$col_id==(col_id_use)
+                                               ,"state"]),
+                                  collapse = ""), pattern = "L|#")=="#"}
+up_left_diag_lookup <- function(row_id_use, col_id_use){ 
+  stri_extract_first_regex(paste0(unlist(seats[(abs(seats$row_id-row_id_use)==abs(seats$col_id-col_id_use))&
+                                                 seats$row_id<(row_id_use) & 
+                                                 seats$col_id<(col_id_use)
+                                               ,c("row_id", "col_id", "state")] %>% arrange(abs(.$row_id-row_id_use)+abs(.$col_id-col_id_use)) %>% pull(state)),
+                                  collapse = ""), pattern = "L|#")=="#"}
+down_left_diag_lookup <- function(row_id_use, col_id_use){ 
+  stri_extract_first_regex(paste0(unlist(seats[(abs(seats$row_id-row_id_use)==abs(seats$col_id-col_id_use))&
+                                                 seats$row_id>(row_id_use) & 
+                                                 seats$col_id<(col_id_use)
+                                               ,c("row_id", "col_id", "state")] %>% arrange(abs(.$row_id-row_id_use)+abs(.$col_id-col_id_use)) %>% pull(state)),
+                                  collapse = ""), pattern = "L|#")=="#"}
+up_right_diag_lookup <- function(row_id_use, col_id_use){ 
+  stri_extract_first_regex(paste0(unlist(seats[(abs(seats$row_id-row_id_use)==abs(seats$col_id-col_id_use))&
+                                                 seats$row_id<(row_id_use) & 
+                                                 seats$col_id>(col_id_use)
+                                               ,c("row_id", "col_id", "state")] %>% arrange(abs(.$row_id-row_id_use)+abs(.$col_id-col_id_use)) %>% pull(state)),
+                                  collapse = ""), pattern = "L|#")=="#"}
+down_right_diag_lookup <- function(row_id_use, col_id_use){ 
+  stri_extract_first_regex(paste0(unlist(seats[(abs(seats$row_id-row_id_use)==abs(seats$col_id-col_id_use))&
+                                                 seats$row_id>(row_id_use) & 
+                                                 seats$col_id>(col_id_use)
+                                               ,c("row_id", "col_id", "state")] %>% arrange(abs(.$row_id-row_id_use)+abs(.$col_id-col_id_use)) %>% pull(state)),
+                                  collapse = ""), pattern = "L|#")=="#"}
+
+vleft_row_occ_lookup <- Vectorize(left_row_occ_lookup)
+vright_row_occ_lookup <- Vectorize(right_row_occ_lookup)
+vup_col_occ_lookup <- Vectorize(up_col_occ_lookup)
+vdown_col_occ_lookup <- Vectorize(down_col_occ_lookup)
+vup_left_diag_lookup <- Vectorize(up_left_diag_lookup)
+vdown_left_diag_lookup <- Vectorize(down_left_diag_lookup)
+vup_right_diag_lookup <- Vectorize(up_right_diag_lookup)
+vdown_right_diag_lookup <- Vectorize(down_right_diag_lookup)
+
 for(i in 1:1000){
-  seats <-
+  df <-
     seats %>%
-    rowwise() %>% 
-    mutate(left_row_occ = 
-             stri_extract_last_regex(paste0(unlist(.[.$row_id==(row_id) &
-                                                        .$col_id<(col_id)
-                                                      ,"state"]),
-                                             collapse = ""), pattern = "L|#")=="#",
-           right_row_occ = 
-             stri_extract_first_regex(paste0(unlist(.[.$row_id==(row_id) &
-                                                        .$col_id>(col_id)
-                                                      ,"state"]),
-                                             collapse = ""), pattern = "L|#")=="#",
-           up_col_occ =  
-             stri_extract_last_regex(paste0(unlist(.[.$row_id<(row_id) &
-                                                        .$col_id==(col_id)
-                                                      ,"state"]),
-                                             collapse = ""), pattern = "L|#")=="#",
-           down_col_occ = 
-             stri_extract_first_regex(paste0(unlist(.[.$row_id>(row_id) &
-                                                        .$col_id==(col_id)
-                                                      ,"state"]),
-                                             collapse = ""), pattern = "L|#")=="#",
-           up_left_diag = 
-             stri_extract_last_regex(paste0(unlist(.[(abs(.$row_id-row_id)==abs(.$col_id-col_id))&
-                                                        .$row_id<(row_id) & 
-                                                        .$col_id<(col_id)
-                                                      ,c("row_id", "col_id", "state")] %>% arrange(abs(.$row_id-row_id)+abs(.$col_id-col_id)) %>% pull(state)),
-                                             collapse = ""), pattern = "L|#")=="#",
-           down_left_diag = 
-             stri_extract_first_regex(paste0(unlist(.[(abs(.$row_id-row_id)==abs(.$col_id-col_id))&
-                                                        .$row_id>(row_id) & 
-                                                        .$col_id<(col_id)
-                                                      ,c("row_id", "col_id", "state")] %>% arrange(abs(.$row_id-row_id)+abs(.$col_id-col_id)) %>% pull(state)),
-                                             collapse = ""), pattern = "L|#")=="#",
-           up_right_diag = 
-             stri_extract_last_regex(paste0(unlist(.[(abs(.$row_id-row_id)==abs(.$col_id-col_id))&
-                                                        .$row_id<(row_id) & 
-                                                        .$col_id>(col_id)
-                                                      ,c("row_id", "col_id", "state")] %>% arrange(abs(.$row_id-row_id)+abs(.$col_id-col_id)) %>% pull(state)),
-                                             collapse = ""), pattern = "L|#")=="#",
-           down_right_diag = 
-             stri_extract_first_regex(paste0(unlist(.[(abs(.$row_id-row_id)==abs(.$col_id-col_id))&
-                                                        .$row_id>(row_id) & 
-                                                        .$col_id>(col_id)
-                                                      ,c("row_id", "col_id", "state")] %>% arrange(abs(.$row_id-row_id)+abs(.$col_id-col_id)) %>% pull(state)),
-                                             collapse = ""), pattern = "L|#")=="#")
+    mutate(left_row_occ = vleft_row_occ_lookup(row_id, col_id),
+           right_row_occ = vright_row_occ_lookup(row_id, col_id),
+           up_col_occ = vup_col_occ_lookup(row_id, col_id),
+           down_col_occ = vdown_col_occ_lookup(row_id, col_id),
+           up_left_diag = vup_left_diag_lookup(row_id, col_id),
+           down_left_diag = vdown_left_diag_lookup(row_id, col_id),
+           up_right_diag = vup_right_diag_lookup(row_id, col_id),
+           down_right_diag = vdown_right_diag_lookup(row_id, col_id))
   
-  seats$total_see <- rowSums(seats %>% select(left_row_occ:down_right_diag), na.rm = T)
-  seats <- 
-    seats %>% 
+  df$total_see <- rowSums(df %>% select(left_row_occ:down_right_diag), na.rm = T)
+  df <- 
+    df %>% 
     mutate(
       state_2 = if_else(state=="L"&total_see<1, "#", state),
       state_2 = if_else(state=="#"&total_see>=5,"L", state_2))
   
-  if(all(seats$state==seats$state_2)){break}
+  if(all(df$state==df$state_2)){break}
   
-  print(paste("count:",i,"occupied_seats:", sum(seats$state_2=="#")))
-  seats$state <- seats$state_2
+  print(paste("count:",i,"occupied_seats:", sum(df$state_2=="#")))
+  df$state <- df$state_2
+  
+  seats <- df
   
 }
 
 sum(seats$state_2=="#")
 
 View(seats %>% select(row_id, col_id, state_2) %>% pivot_wider(names_from =col_id, values_from = state_2))
+
