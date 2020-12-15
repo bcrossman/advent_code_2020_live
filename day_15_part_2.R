97cc831471e554585ebe0c94b8169a63fb556402
Brent Crossman <brent.crossman@gmail.com>
2020-12-15 09:57:04 -0500

    Update day_15_part_2.R

--- day_15_part_2.R
+++ day_15_part_2.R
@@ -3,13 +3,25 @@ start <- as.numeric(read_lines("day15.txt") %>% str_split(",", simplify = T))
 
 # start <- as.numeric(c("0,3,6") %>% str_split(",", simplify = T))
 
-for(i in (length(start)+1):30000000){
-  #i <- 8
-  last_number <- start[i-1]
-  places_spoken <- which(start[1:(i-2)]==last_number)
-  number_spoken <- ifelse(length(places_spoken)==0,0,(i-1)-max(places_spoken))
-  start <- c(start, number_spoken)
-  print(number_spoken)
+running_df <- data.frame("running_numbers" = start, "last_stated" = seq(1:length(start)))
+running_df$last_last_stated <- c(running_df$last_stated[1:6],NA)
+last_number <- tail(start,1)
+
+for(i in (length(start)+1):2020){
+  # i <- 9
+  last_said <- which(running_df$running_numbers==last_number)
+  if(length(last_said)==0){
+    running_df <- 
+      bind_rows(running_df, 
+                data.frame("running_numbers"=last_number, "last_stated"=(i-1), "last_last_stated" = NA))
+    last_said <- which(running_df$running_numbers==last_number)
+  }
+  last_number <- running_df$last_last_stated[last_said]
+  last_number[is.na(last_number)] <- 0
+  running_df$last_last_stated <- running_df$last_stated
+  running_df$last_stated[which(running_df$running_numbers==last_number)] <- i
+  # print(last_number)
+  
 }
 
-number_spoken
+last_number
