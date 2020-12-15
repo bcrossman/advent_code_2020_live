4b624a63e14eb757c6cf5c3ad6dad1362947fecd
Brent Crossman <brent.crossman@gmail.com>
2020-12-15 08:14:07 -0500

    day15

---
+++ day_15_part_2.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+start <- as.numeric(read_lines("day15.txt") %>% str_split(",", simplify = T))
+
+# start <- as.numeric(c("0,3,6") %>% str_split(",", simplify = T))
+
+for(i in (length(start)+1):30000000){
+  #i <- 8
+  last_number <- start[i-1]
+  places_spoken <- which(start[1:(i-2)]==last_number)
+  number_spoken <- ifelse(length(places_spoken)==0,0,(i-1)-max(places_spoken))
+  start <- c(start, number_spoken)
+  print(number_spoken)
+}
+
+number_spoken
