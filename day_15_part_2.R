cf5c820be2e3f752222e7908df8bd96912f8c8a5
Brent Crossman <brent.crossman@gmail.com>
2020-12-15 13:04:01 -0500

    Update day_15_part_2.R

--- day_15_part_2.R
+++ day_15_part_2.R
@@ -3,28 +3,31 @@ start <- as.numeric(read_lines("day15.txt") %>% str_split(",", simplify = T))
 
 # start <- as.numeric(c("0,3,6") %>% str_split(",", simplify = T))
 
-running_df <- data.frame("running_numbers" = start, "last_stated" = seq(1:length(start)))
-running_df$last_last_stated <- c(running_df$last_stated[1:(length(start)-1)],NA)
-last_number <- tail(start,1)
+n <- 3e+07
+# n <- 2020
 
-for(i in (length(start)+1):30000000){
-  # if(i==5){break}
-  last_said <- which(running_df$running_numbers==last_number)
-  if(length(last_said)==0){
-    running_df <- 
-      bind_rows(running_df, 
-                data.frame("running_numbers"=last_number, "last_stated"=(i-1), "last_last_stated" = NA))
-    last_said <- which(running_df$running_numbers==last_number)
-  }
-  last_number <- (i-1) - running_df$last_last_stated[last_said]
-  if(is.na(last_number)){
-    last_number <- 0
-  }
-  running_df$last_last_stated <- running_df$last_stated
-  running_df$last_stated[which(running_df$running_numbers==last_number)] <- i
-  # print(last_number)
+
+complete_possible_set <- data.frame("running_numbers" = (0:n))
+running_df <- data.frame("running_numbers" = start, "when_stated" = seq(1:length(start)))
+
+complete_possible_set <- 
+  complete_possible_set %>% 
+  left_join(running_df )
+
+complete_possible_set$when_stated[19] <- NA
+
+currnum <- start[length(start)]
+
+##Initially did as a dataframe, very slow....
+
+complete_possible_set_m <- as.matrix(complete_possible_set)
+
+for (i in (length(start)):(n-1)) {
+  if(is.na(complete_possible_set_m[currnum+1,2])){new_num <- 0}else{
+    new_num <-  i - complete_possible_set_m[currnum + 1,2]} 
+  complete_possible_set_m[currnum + 1,2] <- i
+  currnum <- new_num
   if(i%%3e+05==0){print(i)}
-  
 }
 
-last_number
+currnum
