81a353b58f38cf32bb95083474cc5595ab63f813
Brent Crossman <brent.crossman@gmail.com>
2020-12-18 05:21:00 -0500

    Update day_18_part_2.R

--- day_18_part_2.R
+++ day_18_part_2.R
@@ -2,12 +2,12 @@ library(tidyverse)
 
 data <- readLines("day18.txt")
 # data <- "1 + 2 * 3 + 4 * 5 + 6"
-"%*%" <- function(x,y){x*y}
-# "%+%" <- function(x,y){x+y}
+# "%*%" <- function(x,y){x*y}
+"%+%" <- function(x,y){x+y}
 evaluate <- function(x){eval(parse(text=x))}
 
-data <- gsub(pattern = "*", replacement = "%*%", x = data, fixed = T)
-# data <- gsub(pattern = "+", replacement = "%+%", x = data, fixed = T)
+# data <- gsub(pattern = "*", replacement = "%*%", x = data, fixed = T)
+data <- gsub(pattern = "+", replacement = "%+%", x = data, fixed = T)
 
 sum(map_dbl(data, evaluate))
 
